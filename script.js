02060f11c7cf787add1f685f728c88bf3094c3df
Willykgafela <willytshepho@gmail.com>
2025-08-21 04:13:25 -0700

    sediba
    new version

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("Sediba Sa Bophelo site loaded.");
+
+// Open lightbox
+function openLightbox(img) {
+  document.getElementById('lightbox').style.display = 'flex';
+  document.getElementById('lightbox-img').src = img.src;
+  document.getElementById('lightbox-img').alt = img.alt;
+}
+
+// Close lightbox
+function closeLightbox() {
+  document.getElementById('lightbox').style.display = 'none';
+}
+
+// Close lightbox on background click
+document.getElementById('lightbox').addEventListener('click', function(e) {
+  if (e.target.id === 'lightbox') {
+    closeLightbox();
+  }
+});
